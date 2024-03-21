b5b6755b5a3411cc4b83037cb306186dacf7b6b8
Sud0Us3r <ivandezad@gmail.com>
2024-03-22 00:08:48 +0100

    Improved Log Logic

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ logLevel LOGLEVEL=ERROR;
 //Funcion que dado un nivel donde mostrar y un mensaje muestra ese mensaje dependiendo de la configuracion de depuracion y el nivel
 //de depuracion recibido
 void log(int logValue, string mensaje) {
-
   //En el caso de que lo que recibamos sea un error, siempre mostraremos por pantalla a no ser que  esta funcion no se haya definido al compilar
   if(logValue == ERROR){
     string prefix = "[\x1b[31mERROR\x1b[0m] ";
@@ -27,26 +26,46 @@ void log(int logValue, string mensaje) {
   }
 
   //Para el warning
-  if(logValue == WARNING && LOGLEVEL == INFO || LOGLEVEL == DEBUG || LOGLEVEL == WARNING){
+  if(logValue == WARNING && (LOGLEVEL == INFO || LOGLEVEL == DEBUG || LOGLEVEL == WARNING)){
     string prefix = "[\x1b[33mWARNING\x1b[0m] ";
     string output = prefix + mensaje;
     cout << output << endl;
   }
 
   //Debug
-  if(logValue == DEBUG && LOGLEVEL == INFO || LOGLEVEL == INFO){
-    prefix = "[\x1b[35mDEBUG\x1b[0m] ";
-    output = prefix + mensaje;
+  if(logValue == DEBUG && (LOGLEVEL == DEBUG || LOGLEVEL == INFO)){
+    string prefix = "[\x1b[35mDEBUG\x1b[0m] ";
+    string output = prefix + mensaje;
     cout << output << endl;
   }
 
   //En caso de recibir que sea info solo la mostraremos en el caso de que las opciones de depuracion sean 
   //las de informacion
   if(logValue == INFO && LOGLEVEL == INFO){
-    prefix = "[\x1b[36mINFO\x1b[0m] ";
-    output = prefix + mensaje;
+    string prefix = "[\x1b[36mINFO\x1b[0m] ";
+    string output = prefix + mensaje;
     cout << output << endl;
   }
 
 }
 #endif
+
+#ifndef LOG
+#define LOGLEVEL ERROR
+enum logLevel {
+  ERROR,
+  WARNING,
+  DEBUG,
+  INFO
+};
+void log(int num, string message) {
+
+}
+#endif
+
+int main(){
+  log(INFO, "Esto es un mensaje de info");
+  log(DEBUG, "Esto es un mensaje debug");
+  log(WARNING, "Esto es un mensaje de warning");
+  log(ERROR, "Esto es un mensaje de error");
+}
