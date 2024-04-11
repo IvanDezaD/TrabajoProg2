899b969546b9c83832af1b75c3d3ee9acebcf17c
kali <kali@kali.kali>
2024-04-11 19:12:11 +0200

    test

--- src/backtrack.cpp
+++ src/backtrack.cpp
@@ -4,5 +4,7 @@
 
 //NOTE: importante destacar que en esta implementacion, el tamaño es NxM, pero en realidad la matriz de verdad es N+2xM+2 ya que tenemos los indicadores de alturas, pero esos posteriormente los ignoramos, sencillamente llamando a la funcion con un +1
 bool resolverTablero(tablero *miTablero) {
-  return true;
+  if estaResuelto(miTablero) {
+    return true;
+  }
 }
